634f06d2d5521efce6b897e751994e7587d0bc63
sanjuy <kumar.sanju056@gmail.com>
2025-02-20 17:45:08 +0530

    comment file upload code

--- src/main/java/com/gtbsa/LMS/helper/FileUploadHelper.java
+++ src/main/java/com/gtbsa/LMS/helper/FileUploadHelper.java
@@ -1,35 +1,27 @@
-package com.gtbsa.LMS.helper;
+// package com.gtbsa.LMS.helper;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+// import org.springframework.stereotype.Component;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
-
-@Component
-public class FileUploadHelper {
+// @Component
+// public class FileUploadHelper {
     
-    // public final String uploadDir = "src/main/resources/static/uploadFiles";
-    public final String uploadDir = new ClassPathResource("/static/uploadFiles").getFile().getAbsolutePath();
+//     // public final String uploadDir = "src/main/resources/static/uploadFiles";
+//     public final String uploadDir = new ClassPathResource("/static/uploadFiles").getFile().getAbsolutePath();
 
-    public FileUploadHelper() throws IOException
-    {
+//     public FileUploadHelper() throws IOException
+//     {
         
-    }
+//     }
 
-    public boolean uploadFile(MultipartFile file) {
-        boolean f = false;
+//     public boolean uploadFile(MultipartFile file) {
+//         boolean f = false;
 
-        try{
-            Files.copy(file.getInputStream(), Paths.get(uploadDir+File.separator+file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
-            f = true;
-        } catch (IOException e) {
-        }
+//         try{
+//             Files.copy(file.getInputStream(), Paths.get(uploadDir+File.separator+file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+//             f = true;
+//         } catch (IOException e) {
+//         }
 
-        return f;
-    }
-}
+//         return f;
+//     }
+// }
--- src/main/java/com/gtbsa/LMS/restControllers/FileUploadController.java
+++ src/main/java/com/gtbsa/LMS/restControllers/FileUploadController.java
@@ -1,43 +1,43 @@
-package com.gtbsa.LMS.restControllers;
+// package com.gtbsa.LMS.restControllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.multipart.MultipartFile;
+// import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.gtbsa.LMS.helper.FileUploadHelper;
+// import com.gtbsa.LMS.helper.FileUploadHelper;
 
 
-@RestController
-@RequestMapping("/api/v2")
-public class FileUploadController {
+// @RestController
+// @RequestMapping("/api/v2")
+// public class FileUploadController {
     
-    @Autowired
-    private FileUploadHelper fileHelper;
+//     @Autowired
+//     private FileUploadHelper fileHelper;
 
 
-    @SuppressWarnings("null")
-    @PostMapping("/upload-file")
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+//     @SuppressWarnings("null")
+//     @PostMapping("/upload-file")
+//     public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
         
-        if (file.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("file could not be empty");
-        }
-        if (!file.getContentType().equals("image/jpeg")) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("only jpeg file");
-        }
-        // upload file method-=-=-=
-        boolean vl = fileHelper.uploadFile(file);
-        if (vl) {
-            // return ResponseEntity.ok("file upload success");
-            return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/uploadFiles/").path(file.getOriginalFilename()).toUriString());
-        }
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-    }
+//         if (file.isEmpty()) {
+//             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("file could not be empty");
+//         }
+//         if (!file.getContentType().equals("image/jpeg")) {
+//             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("only jpeg file");
+//         }
+//         // upload file method-=-=-=
+//         boolean vl = fileHelper.uploadFile(file);
+//         if (vl) {
+//             // return ResponseEntity.ok("file upload success");
+//             return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/uploadFiles/").path(file.getOriginalFilename()).toUriString());
+//         }
+//         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+//     }
     
-}
+// }
